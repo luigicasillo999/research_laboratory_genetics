c74c81d139f6d64bf6a11eb1bca5b711aa0eca44
luigicasillo999 <luigi.casillo.work@gmail.com>
2025-11-19 16:19:03 +0100

    add quality_controls.sh with just standard gwas qc

---
+++ scripts/quality_controls.sh
@@ -0,0 +1,51 @@
+#!/bin/bash -e
+
+# ----------------------------------------
+# create directories where to store results
+# ----------------------------------------
+
+mkdir -p results/quality_controls results/genotypes_quality_checked
+
+# ----------------------------------------
+# gwas standard quality controls
+# ----------------------------------------
+
+# remove variants with MAF < 0.01, HWE deviation p-value < 1e-6, genotyping call rate < 0.01
+# remove subjects with sample call rate < 0.01
+# write the list of remaining variants and a list of the remaining subjects
+plink \
+  --bfile data/genotypes/samples \
+  --maf 0.01 \
+  --hwe 1e-6 \
+  --geno 0.01 \
+  --mind 0.01 \
+  --write-snplist \
+  --make-just-fam \
+  --out results/quality_controls/samples_standard_qced
+
+# ----------------------------------------
+# remove subjects with exceding heterozygosity
+# ----------------------------------------
+
+# get a list of the independent variants
+
+
+# compute subjects' heterozygoisty rate using independent variants
+
+
+# remove individuals with heterozygisity rate > 3 standard deviation in R
+
+
+# ----------------------------------------
+# remove relatives
+# ----------------------------------------
+
+# remove closely related individuals
+
+
+# ----------------------------------------
+# generate final files
+# ----------------------------------------
+
+# generate final fileset including quality checked variants and subjects
+
